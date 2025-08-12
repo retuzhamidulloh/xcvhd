3cb9111a4b7daa7f8b32b7739ed32ef38a3436fe
retuzhamidulloh <retuz416@gmail.com>
2025-08-12 13:16:32 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+alert("Hozir random son chiqadi!");
+
+let son = Math.floor(Math.random() * 50) * 2; 
+alert("Chiqqan son: " + son);
+
+
+let bolish = son / 2;
+let qoldiq = 0; 
+let kopaytirish = son * 2
+alert("2 ga bo‘lgandagi natija: " + bolish + "\nQoldiq: " + qoldiq + "\nKopaytirish:" + kopaytirish );
